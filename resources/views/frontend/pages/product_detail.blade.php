b231ac111da8a5016c89695277f3c4b48e76b20f
Dominique Youness <youness.dominique@gmail.com>
2022-06-14 16:46:05 +0200

    DELETE ADD TO CART IN DETAIL

--- resources/views/frontend/pages/product_detail.blade.php
+++ resources/views/frontend/pages/product_detail.blade.php
@@ -332,13 +332,13 @@
                                         </a>
                                         <div class="button-head">
                                             <div class="product-action">
-                                                <a data-toggle="modal" data-target="#modelExample" title="Quick View" href="#"><i class=" ti-eye"></i><span>Quick Shop</span></a>
-                                                <a title="Wishlist" href="#"><i class=" ti-heart "></i><span>Add to Wishlist</span></a>
-                                                <a title="Compare" href="#"><i class="ti-bar-chart-alt"></i><span>Add to Compare</span></a>
+                                                <a data-toggle="modal" data-target="#modelExample" title="Quick View" href="#">Voir Plus<i class=" ti-eye"></i><span>Quick Shop</span></a>
+{{--                                                <a title="Wishlist" href="#"><i class=" ti-heart "></i><span>Add to Wishlist</span></a>--}}
+{{--                                                <a title="Compare" href="#"><i class="ti-bar-chart-alt"></i><span>Add to Compare</span></a>--}}
                                             </div>
-                                            <div class="product-action-2">
-                                                <a title="Add to cart" href="#">Add to cart</a>
-                                            </div>
+{{--                                            <div class="product-action-2">--}}
+{{--                                                <a title="Add to cart" href="#">Add to cart</a>--}}
+{{--                                            </div>--}}
                                         </div>
                                     </div>
                                     <div class="product-content">
