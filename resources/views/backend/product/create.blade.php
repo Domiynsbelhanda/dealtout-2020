8288d18a075a72e14af4680e393ee5fa431bbbb7
Dominique Youness <youness.dominique@gmail.com>
2022-08-12 11:31:05 +0200

    [FIX] Jquery test 5

--- resources/views/backend/product/create.blade.php
+++ resources/views/backend/product/create.blade.php
@@ -158,27 +158,27 @@
 <script src="{{asset('public/vendor/laravel-filemanager/js/stand-alone-button.js')}}"></script>
 <script src="{{asset('backend/summernote/summernote.min.js')}}"></script>
 
-<script>
-    $('#lfm').filemanager('image');
+{{--<script>--}}
+{{--    $('#lfm').filemanager('image');--}}
 
-    $(document).ready(function() {
-      $('#summary').summernote({
-        placeholder: "Write short description.....",
-          tabsize: 2,
-          height: 100
-      });
-    });
+{{--    $(document).ready(function() {--}}
+{{--      $('#summary').summernote({--}}
+{{--        placeholder: "Write short description.....",--}}
+{{--          tabsize: 2,--}}
+{{--          height: 100--}}
+{{--      });--}}
+{{--    });--}}
 
-    $(document).ready(function() {
-      $('#description').summernote({
-        placeholder: "Write detail description.....",
-          tabsize: 2,
-          height: 150
-      });
-    });
-    // $('select').selectpicker();
+{{--    $(document).ready(function() {--}}
+{{--      $('#description').summernote({--}}
+{{--        placeholder: "Write detail description.....",--}}
+{{--          tabsize: 2,--}}
+{{--          height: 150--}}
+{{--      });--}}
+{{--    });--}}
+{{--    // $('select').selectpicker();--}}
 
-</script>
+{{--</script>--}}
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script>
   $('#cat_id').change(function(){
